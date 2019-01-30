9a00447a5d3e35a8185b7403660e5789bcb0baf7
Hakan Sukru Dag <hakansukrudag@gmail.com>
2019-01-30 12:32:44 +0000

    9-syntax error-9 concat

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,5 +1,5 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+	return w1.concat(w2);
 }
 
 var result = s('code', 'your');
@@ -7,8 +7,8 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - How many parameters does the function take? 2
+// - What is the function name? concat
+// - Where is the function called? with what parameters? concat(w2)
+// - What does the function return? your
+// - How can this function be improved? The concat() method is used to merge two or more arrays.
