cab794472f01f9ec393d3a3ed22039618f752d56
Hakan Sukru Dag <hakansukrudag@gmail.com>
2019-01-30 12:28:55 +0000

    8-syntax error-8 Math.random

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,14 @@
 function getNumber() {
-    return Math.random() * 10;
+	return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
+// - What does this program do? (Random value)
+// - How many parameters does the function take? 2
+// - What is the function name? (Math.random)
 // - Where is the function called? with what parameters?
+// - function getNumber() {
+// -    return Math.random();
